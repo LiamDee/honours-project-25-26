6aace8bca29452216ff20845d7c71d786e18d968
LiamDee <LDEBUF300@caledonian.ac.uk>
2026-02-16 06:32:50 +0000

    working on layout for main screen

--- primary-research-instrument/developed-app/app/src/main/java/org/me/gcu/focusath/AppAdapter.java
+++ primary-research-instrument/developed-app/app/src/main/java/org/me/gcu/focusath/AppAdapter.java
@@ -25,16 +25,16 @@ public class AppAdapter extends ArrayAdapter<AppDetails> {
         }
         TextView app_name_text = convertView.findViewById(R.id.app_name_text);
         TextView usage_time_text = convertView.findViewById(R.id.usage_time_text);
-        TextView usage_percent_text = convertView.findViewById(R.id.usage_percent_text);
+        //TextView usage_percent_text = convertView.findViewById(R.id.usage_percent_text);
         ImageView appIcon = convertView.findViewById(R.id.appIcon);
-        ProgressBar usageProgressBar = convertView.findViewById(R.id.usageProgressBar);
+        //ProgressBar usageProgressBar = convertView.findViewById(R.id.usageProgressBar);
 
         assert usageStats != null;
         app_name_text.setText(usageStats.appName);
         usage_time_text.setText(usageStats.usageTime);
-        usage_percent_text.setText(usageStats.usagePercent + "%");
+        //usage_percent_text.setText(usageStats.usagePercent + "%");
         appIcon.setImageDrawable(usageStats.appIcon);
-        usageProgressBar.setProgress(usageStats.usagePercent);
+        //usageProgressBar.setProgress(usageStats.usagePercent);
 
         return convertView;
 
